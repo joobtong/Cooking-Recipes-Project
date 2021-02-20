f8062a1b086b89202a24508904b5972d043d05e0
joobtong <49297737+joobtong@users.noreply.github.com>
2021-02-21 06:36:19 +0700

    add bottom nav
    add bottom nav

--- app/src/main/java/com/example/cooking_recipes_project/MainActivity.kt
+++ app/src/main/java/com/example/cooking_recipes_project/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.cooking_recipes_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment,R.id.searchFragment
+            ,R.id.chatFragment,R.id.boardFragment,R.id.profileFragment
+        ))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
